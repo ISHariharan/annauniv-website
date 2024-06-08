255143f853c95659ea1056be9748ccbb6a1056a1
ISHariharan <ishariharan2004@gmail.com>
2024-06-08 11:43:45 +0530

    push created app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Home from "./components/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element: <Home />
+  },
+]);
+
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Footer from "../Footer/Footer";
+
+
+function Home(){
+    return(
+        <div className="HomeContainer">
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
